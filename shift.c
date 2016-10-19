74c053c0e799a2c3ea4c2a2f041af2ccb647469b
William Asman <wasman@e1z1r2p32.42.us.org>
2016-10-19 14:04:27 -0700

    adding our shift function

---
+++ shift.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   shift.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wasman <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/10/19 12:25:39 by wasman            #+#    #+#             */
+/*   Updated: 2016/10/19 14:03:40 by wasman           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit.h"
+:
+char	*shift(char *tet)
+{
+	int	max;
+	int	len;
+	char	*tmp;
+	int	i;
+
+	max = 0;
+	len = 0;
+	i = 0;
+	while (tet[len] != '\0')
+		len++;
+	tmp = ft_strnew(len);
+	if (tmp == NULL)
+		return (NULL);
+	len--;
+	tmp = ft_memset(tmp, '0', len);
+	if (tet[len] != '1')
+	{
+		while(tmp[i+1] != '\0')
+		{
+			tmp[i + 1] = tet[i];
+			i++;
+		}
+		return (tmp);
+	}
+	return (NULL);
+}
