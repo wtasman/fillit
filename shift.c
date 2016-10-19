8b6a4ced1793f28c04146001d2c2fe60caaddfb6
William Asman <wasman@e1z1r2p32.42.us.org>
2016-10-19 16:08:34 -0700

    added my partners files and fixed a segfault with shift

--- shift.c
+++ shift.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: wasman <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/10/19 12:25:39 by wasman            #+#    #+#             */
-/*   Updated: 2016/10/19 14:16:59 by whickey          ###   ########.fr       */
+/*   Created: 2016/10/19 15:54:12 by wasman            #+#    #+#             */
+/*   Updated: 2016/10/19 15:59:28 by wasman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,8 @@ char	*shift(char *tet)
 	i = 0;
 	while (tet[len] != '\0')
 		len++;
+	if (len == 0)
+		return (NULL);
 	tmp = ft_strnew(len);
 	if (tmp == NULL)
 		return (NULL);
@@ -31,12 +33,20 @@ char	*shift(char *tet)
 	tmp = ft_memset(tmp, '0', len);
 	if (tet[len] != '1')
 	{
-		while(tmp[i+1] != '\0')
+		while(tmp[i + 1] != '\0')
 		{
-			tmp[i + 1] = tet[i];
+			tmp[i] = tet[i];
 			i++;
 		}
 		return (tmp);
 	}
 	return (NULL);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+		ft_putstr(shift(argv[1]));
+	ft_putchar('\n');
+	return (0);
 }
